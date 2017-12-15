230e38816dd3fcd9bb529ffd70bb1d77f09cbe77
ptwitchapon <poonpim0203@gmail.com>
2017-12-15 14:20:51 +0700

    first commit

---
+++ app/src/main/java/com/example/ptwitchapon/familyday/Adapter/MenuAdapter.java
@@ -0,0 +1,8 @@
+package com.example.ptwitchapon.familyday.Adapter;
+
+/**
+ * Created by ptwitchapon on 15/12/2560.
+ */
+
+public class MenuAdapter {
+}
--- app/src/main/java/com/example/ptwitchapon/familyday/ScanQR.java
+++ app/src/main/java/com/example/ptwitchapon/familyday/ScanQR.java
@@ -1,13 +1,39 @@
 package com.example.ptwitchapon.familyday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ScanQR extends AppCompatActivity {
-
+    public static final int REQUEST_QR_SCAN = 4;
+    String TAG = "Poon";
+    EditText edt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
+        ImageView img = (ImageView) findViewById(R.id.qrscan);
+        edt = (EditText) findViewById(R.id.edtsms) ;
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                startActivityForResult(Intent.createChooser(intent, "Scan with"), REQUEST_QR_SCAN);
+            }
+        });
+
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_QR_SCAN && resultCode == RESULT_OK) {
+            String contents = intent.getStringExtra("SCAN_RESULT");
+
+            Log.d("Poon", contents);
+            edt.setText(contents);
+        }
     }
 }
