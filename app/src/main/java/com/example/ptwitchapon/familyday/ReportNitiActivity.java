5446225abe4894f4b400c66d352df9e43c6fd0cf
pmakkaraphon <akkaraphon.pim@gmail.com>
2017-12-25 09:02:47 +0700

    123123121

---
+++ app/src/main/java/com/example/ptwitchapon/familyday/ReportNitiActivity.java
@@ -0,0 +1,16 @@
+package com.example.ptwitchapon.familyday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportNitiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_niti);
+
+
+
+    }
+}
